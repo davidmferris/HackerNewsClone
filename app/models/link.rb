0c1d40d6e47a2282d7855b34b135bc37ffaaa356
Dave Ferris <davidmferris@gmail.com>
2013-07-29 14:47:03 -0700

    initial commit

---
+++ app/models/link.rb
@@ -0,0 +1,10 @@
+class Link < ActiveRecord::Base
+  attr_accessible :title, :url
+
+  has_many :comments
+  has_many :votes
+
+  validates_presence_of :url
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackerNews::Application.config.secret_token = 'f6feb6ad2c14d244ffd5922487238251451b3db23adcd2aff92f5f7291842de0c1693e81d696c5889f4a340141323d35401775871ff3a12e138a10095932a3d2'
